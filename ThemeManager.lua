ff44024a37a0df92b70b1182d02f8853979e8989
xq7n4v9k2z <atthalaalkhairan12@gmail.com>
2026-06-24 07:40:49 +0700

    Update ThemeManager.lua

--- ThemeManager.lua
+++ ThemeManager.lua
@@ -64,23 +64,8 @@ local ThemeManager = {} do
         }
     }
     ThemeManager.BuiltInThemes = {
-        ["ZalStore"]    = { 1, httpService:JSONDecode([[{"FontColor":"ffffff","MainColor":"101010","AccentColor":"007dff","BackgroundColor":"111111","OutlineColor":"000000"}]]) },
-        ["Default"] 		= { 2, httpService:JSONDecode([[{"FontColor":"ffffff","MainColor":"191919","AccentColor":"7d55ff","BackgroundColor":"0f0f0f","OutlineColor":"000000"}]]) },
-        ["GameSense"]       = { 3, httpService:JSONDecode([[{"FontColor":"ffffff","MainColor":"101010","AccentColor":"9CB819","BackgroundColor":"111111","OutlineColor":"000000"}]]) },
-        ["Comet.pub"]       = { 4, httpService:JSONDecode([[{"FontColor":"ffffff","MainColor":"0F0F0F","AccentColor":"5D589D","BackgroundColor":"0F0F0F","OutlineColor":"000000"}]]) },  
-        ["Tokyohook.cc"]    = { 5, httpService:JSONDecode([[{"FontColor":"ffffff","MainColor":"191925","AccentColor":"6759b3","BackgroundColor":"16161f","OutlineColor":"000000"}]]) },
-        ["White and black"]  = { 6, httpService:JSONDecode([[{"FontColor":"ffffff","MainColor":"060606","AccentColor":"FFFFFF","BackgroundColor":"09090a","OutlineColor":"000000"}]]) },
-        ["Red and black"]  = { 7, httpService:JSONDecode([[{"FontColor":"ffffff","MainColor":"060606","AccentColor":"ff0000","BackgroundColor":"09090a","OutlineColor":"000000"}]]) },
-        ["Green and black"]  = { 8, httpService:JSONDecode([[{"FontColor":"ffffff","MainColor":"060606","AccentColor":"00ff00","BackgroundColor":"09090a","OutlineColor":"000000"}]]) },                                
-        ["Pink and black"]  = { 9, httpService:JSONDecode([[{"FontColor":"ffffff","MainColor":"0f0f0f","AccentColor":"ff0082","BackgroundColor":"121212","OutlineColor":"000000"}]]) },
-        ["Purple and black"]  = { 10, httpService:JSONDecode([[{"FontColor":"ffffff","MainColor":"060606","AccentColor":"b500ff","BackgroundColor":"09090a","OutlineColor":"000000"}]]) },
-        ["Modern Ui"]  = { 11, httpService:JSONDecode([[{"FontColor":"ffffff","MainColor":"060606","AccentColor":"ffd28f","BackgroundColor":"09090a","OutlineColor":"000000"}]]) },
-        ["Blox fruit"]  = { 12, httpService:JSONDecode([[{"FontColor":"ffffff","MainColor":"060606","AccentColor":"9797f7","BackgroundColor":"09090a","OutlineColor":"000000"}]]) },
-        ["Teal and black"]  = { 13, httpService:JSONDecode([[{"FontColor":"ffffff","MainColor":"060606","AccentColor":"00ffff","BackgroundColor":"09090a","OutlineColor":"000000"}]]) },
-        ["Midnight blue"]  = { 14, httpService:JSONDecode([[{"FontColor":"ffffff","MainColor":"0f142b","AccentColor":"0000ff","BackgroundColor":"00001d","OutlineColor":"000000"}]]) },
-        ["Midnight black and red"]  = { 15, httpService:JSONDecode([[{"FontColor":"ffffff","MainColor":"0b0b0b","AccentColor":"f60000","BackgroundColor":"080707","OutlineColor":"000000"}]]) },
-        ["Yama cod theme"]  = { 16, httpService:JSONDecode([[{"FontColor":"000000","MainColor":"e2e2e2","AccentColor":"000000","BackgroundColor":"d4d4d4","OutlineColor":"000000"}]]) },
-        ["Red ice"]  = { 17, httpService:JSONDecode([[{"FontColor":"ffffff","MainColor":"0e0e0e","AccentColor":"ffffff","BackgroundColor":"220000","OutlineColor":"000000"}]]) },
+        ["ZalStore"]  = { 1, httpService:JSONDecode([[{"FontColor":"ffffff","MainColor":"060606","AccentColor":"0045ff","BackgroundColor":"09090a","OutlineColor":"000000"}]]) },
+        ["ZalStore 2"]  = { 2, httpService:JSONDecode([[{"FontColor":"ffffff","MainColor":"0b0a0e","AccentColor":"0045ff","BackgroundColor":"0e0e14","OutlineColor":"000000"}]]) },
     }
 
     function ThemeManager:SetLibrary(library)
